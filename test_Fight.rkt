dcb98e99943d257a1d95ece78a42fd0d8f6ca23b
dstorrs <dstorrs.public@gmail.com>
2020-05-07 07:22:57 -0400

    simplied test-heroes.csv, tested make-fighter

--- test_Fight.rkt
+++ test_Fight.rkt
@@ -8,15 +8,23 @@
 (define-values (heroes-rows villains-rows) (get-csv-data))
 
 (test-suite
- "make-combatant"
+ "make-fighter"
 
  (define-values (headers buff-field-names fields num-fields)
-  (parse-csv-data heroes-rows))
+   (parse-csv-data heroes-rows))
 
- (define data (cdr heroes-rows))
- (define fighter (make-fighter (car data) headers buff-field-names fields num-fields))
- (is fighter
-     (combatant "Ami" 13993 0 1 0 0 1 "Kei" ""
-                (list (buff "Mori" '("Ami" "Kei" "Naruto 01" "Prime") 0.06 0.06))
-                '() '() 13993 0.3 0.3 14 3))
- )
+ (for ([row (cdr heroes-rows)]
+       [expected-name '("Ami" "Kei"  "Prime" "Naruto 01")]
+       [correct (list (combatant "Ami" 13993 0 1 0 0 1 "Kei" ""
+                                 (list (buff "Mori" '("Ami" "Kei" "Naruto 01" "Prime") 0.06 0.06))
+                                 '() '() 13993 0.3 0.3 14 3)
+                      (combatant "Kei" 5785 0 1 0 0 1 "" "" '() '() '() 5785 0.3 0.3 6 3)
+                      (combatant "Prime" 17854 0 1 0.15 0 1 "" "" (list (buff "Teamwork" '("Naruto 01" "Prime") 0.1 0.1)) '() '() 17854 0.44999999999999996 0.3 18 3)
+                      (combatant "Naruto 01" 17854 0 1 0.15 0 1 "Prime" "Prime" (list (buff "Teamwork" '("Naruto 01" "Prime") 0.1 0.1)) '() '() 17854 0.44999999999999996 0.3 18 3)
+
+                      )]
+       )
+   (is (make-fighter row headers buff-field-names fields num-fields)
+       correct
+       (format "correctly made fighter ~a" expected-name))))
+
