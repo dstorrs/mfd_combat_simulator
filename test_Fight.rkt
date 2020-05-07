e600e57b6d9f323e1d0372e57036a1810b20e30a
dstorrs <dstorrs.public@gmail.com>
2020-05-07 08:33:31 -0400

    extended test_Fight.rkt

--- test_Fight.rkt
+++ test_Fight.rkt
@@ -16,7 +16,6 @@
 
  ; NOTE:  We are testing make-fighter, so buffs will not have been applied
  (for ([row (cdr heroes-rows)]
-       [expected-name '("Ami" "Kei"  "Prime" "Naruto 01" "Naruto 02" "Naruto 03" "Naruto 04")]
        [correct (list
                  ;          Name,XP,BonusXP,BonusHP,BonusToHit,BonusToDefend,AOE,BodyguardFor,LinkedTo,BuffName,BuffWho,BuffOffense,BuffDefense
                  (combatant "Ami" 13993 0 1 0 0 1 "Kei" ""
@@ -48,21 +47,21 @@
                             (list (buff "Teamwork" '("Naruto 01" "Naruto 02" "Naruto 03" "Naruto 04" "Naruto 05" "Naruto 06" "Naruto 07" "Prime") 0.1 0.23))
                             '() '() 17854 0.44999999999999996 0.3 18 3)
 )]
-       [expected-bgf  (list "Kei" "" "" "Prime"  "Prime" "Prime" "Prime")]
-       [expected-lt   (list "" "" "" "Prime" "Prime" "Prime" "Prime")]
+       [expected-bgf  (list "Kei" "" "" "Prime"  "Prime" "Prime" "Prime" "Prime"  "Prime" "Prime")]
+       [expected-lt   (list "" "" "" "Prime"  "Prime" "Prime" "Prime" "Prime"  "Prime" "Prime")]
        )
    (define fighter (make-fighter row headers buff-field-names fields num-fields))
    (is fighter
        correct
-       (format "correctly made fighter ~a" expected-name))
-   ;; ; Verify that these came out in the right order and my test data wasn't backwards 
-   ;; (is (combatant.BodyguardFor fighter)
-   ;;     expected-bgf
-   ;;     "bodyguard-for correct"
-   ;;     )
-   ;; (is (combatant.LinkedTo fighter)
-   ;;     expected-lt
-   ;;     "linked-to correct")
+       (format "correctly made fighter ~a" (combatant.Name fighter)))
+   ; These were already tested implicitly, but put them here for human readability
+   (is (combatant.BodyguardFor fighter)
+       expected-bgf
+       "bodyguard-for correct"
+       )
+   (is (combatant.LinkedTo fighter)
+       expected-lt
+       "linked-to correct")
    ))
 
 #;
